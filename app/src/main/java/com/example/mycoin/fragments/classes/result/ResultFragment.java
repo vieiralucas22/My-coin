9c9e250f02855514aaa525b53c5442d9a76ffc3c
Lucas Vieira Brito <lucasvieirabrito@MacBook-Air-de-Lucas.local>
2024-05-26 14:31:41 -0300

    UI da tela de resultados feita

--- app/src/main/java/com/example/mycoin/fragments/classes/result/ResultFragment.java
+++ app/src/main/java/com/example/mycoin/fragments/classes/result/ResultFragment.java
@@ -1,20 +1,18 @@
 package com.example.mycoin.fragments.classes.result;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.mycoin.R;
+import com.example.mycoin.fragments.BaseFragment;
 
-public class ResultFragment extends Fragment {
+public class ResultFragment extends BaseFragment {
 
     private ResultViewModel mViewModel;
 
@@ -26,13 +24,6 @@ public class ResultFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_result, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(ResultViewModel.class);
-        // TODO: Use the ViewModel
     }
 
 }
